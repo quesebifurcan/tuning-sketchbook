8a132ddf17c3c66682550ef8cea2cba2bf044c50
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2016-10-24 18:47:34 +0200

    tuning: include "inverted" ratios

--- tuning.py
+++ tuning.py
@@ -4,9 +4,11 @@ from fractions import Fraction
 OCTAVE = Fraction(2, 1)
 
 
-def limit_fraction(f, limit):
-    if f >= limit:
-        return limit_fraction(f / limit, limit)
+def limit_fraction(f):
+    if f >= 2:
+        return limit_fraction(f / 2)
+    if f < 1:
+        return limit_fraction(f * 2)
     return f
 
 
@@ -14,22 +16,23 @@ def build_scale(ratios):
     combinations = itertools.combinations_with_replacement(ratios, 2)
     pitchclasses = []
     for x, y in combinations:
-        pitchclasses.append(limit_fraction(x * y, OCTAVE))
+        pitchclasses.append(limit_fraction(x * y))
     return sorted(set(pitchclasses))
 
 
 def main():
-    base_ratios = (
+    base_ratios = [
         Fraction(1, 1),
-        Fraction(9, 8),
-        Fraction(5, 4),
-        Fraction(11, 8),
         Fraction(3, 2),
-        Fraction(13, 8),
+        Fraction(5, 4),
         Fraction(7, 4),
-    )
-    scale = build_scale(base_ratios)
-    print(scale)
+        Fraction(11, 8),
+        Fraction(13, 8),
+    ]
+    inverted_ratios = [1/r for r in base_ratios]
+    scale = build_scale(base_ratios + inverted_ratios)
+    for p in scale:
+        print(p)
 
 
 if __name__ == '__main__':
